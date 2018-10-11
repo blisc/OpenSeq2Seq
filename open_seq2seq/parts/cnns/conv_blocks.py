25556f0e0b207845665dd9f5c2838bbf7f5ee99e
blisc <jasonli9@live.ca>
2018-10-10 17:57:24 -0700

    Initial conv_res block puesdocode

--- open_seq2seq/parts/cnns/conv_blocks.py
+++ open_seq2seq/parts/cnns/conv_blocks.py
@@ -42,6 +42,52 @@ def conv_actv(layer_type, name, inputs, filters, kernel_size, activation_fn, str
     output = activation_fn(output)
   return output
 
+def conv_bn_res_bn_actv(layer_type, name, inputs, res_inputs, filters, kernel_size, activation_fn, strides,
+                        padding, regularizer, training, data_format, bn_momentum,
+                        bn_epsilon, dilation=1):
+  layer = layers_dict[layer_type]
+
+  res = layer(res_inputs)
+  res = bn(res)
+
+  conv = layer(
+      name="{}".format(name),
+      inputs=inputs,
+      filters=filters,
+      kernel_size=kernel_size,
+      strides=strides,
+      padding=padding,
+      dilation_rate=dilation,
+      kernel_regularizer=regularizer,
+      use_bias=False,
+      data_format=data_format,
+  )
+
+  # trick to make batchnorm work for mixed precision training.
+  # To-Do check if batchnorm works smoothly for >4 dimensional tensors
+  squeeze = False
+  if layer_type == "conv1d":
+    axis = 1 if data_format == 'channels_last' else 2
+    conv = tf.expand_dims(conv, axis=axis)  # NWC --> NHWC
+    squeeze = True
+
+  bn = tf.layers.batch_normalization(
+      name="{}/bn".format(name),
+      inputs=conv,
+      gamma_regularizer=regularizer,
+      training=training,
+      axis=-1 if data_format == 'channels_last' else 1,
+      momentum=bn_momentum,
+      epsilon=bn_epsilon,
+  )
+
+  if squeeze:
+    bn = tf.squeeze(bn, axis=axis)
+
+  output = bn + res
+  if activation_fn is not None:
+    output = activation_fn(output)
+  return output
 
 def conv_bn_actv(layer_type, name, inputs, filters, kernel_size, activation_fn, strides,
                  padding, regularizer, training, data_format, bn_momentum,
