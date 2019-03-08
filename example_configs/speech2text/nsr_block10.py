716fcaa0d0e1e0d35d1421710d9f9f946cf9ee9c
Jason <jasoli@nvidia.com>
2019-03-08 12:27:09 -0800

    update config
    Signed-off-by: Jason <jasoli@nvidia.com>

--- example_configs/speech2text/nsr_block10.py
+++ example_configs/speech2text/nsr_block10.py
@@ -227,7 +227,7 @@ base_params = {
         "input_type": "logfbank",
         "vocab_file": "open_seq2seq/test_utils/toy_speech_data/vocab.txt",
         "norm_per_feature": False,
-        "window_type": 'hanning',
+        "window_type": "hanning",
         "librosa": False,
         "syn_enable": False,
         "syn_subdirs" :["1_50", "2_44", "3_47", "50", "46", "48"]
