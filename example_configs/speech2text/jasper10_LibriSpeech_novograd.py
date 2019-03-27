ed0c56bd344a1b617cf42f57b35b21a8fb34bc29
blisc <jasonli9@live.ca>
2019-03-26 18:38:51 -0700

    Update jasper10_LibriSpeech_novograd.py

--- example_configs/speech2text/jasper10_LibriSpeech_novograd.py
+++ example_configs/speech2text/jasper10_LibriSpeech_novograd.py
@@ -232,12 +232,13 @@ base_params = {
         "input_type": "logfbank",
         "vocab_file": "open_seq2seq/test_utils/toy_speech_data/vocab.txt",
         "norm_per_feature": True,
-        "window_type": "hamming",
+        "window": "hanning",
         "syn_enable": False,
         "syn_subdirs": ["1_50", "2_44", "3_47", "50", "46", "48"],
         "precompute_mel_basis": True,
         "sample_freq": 16000,
-        "pad_to": 16
+        "pad_to": 16,
+        "dither": 1e-5,
     },
 }
 
@@ -248,7 +249,6 @@ train_params = {
         "dataset_files": dataset_files,
         "max_duration": 16.7,
         "shuffle": True,
-        "dither": 1e-5,
     },
 }
 
